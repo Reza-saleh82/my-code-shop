4211c8bb028eed3e727f632c6c56b9cba6d6ad11
reza <reza28311382.com@gmail.com>
2025-06-08 21:01:49 +0330

    once change

---
+++ src/component/NavSite/NavSite.jsx
@@ -0,0 +1,11 @@
+
+
+function NavSite() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default NavSite;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+
+
+function Home() {
+  return (
+    <>
+
+        <Link to="/">Home</Link>
+    </>
+)
+}
+
+export default Home;
